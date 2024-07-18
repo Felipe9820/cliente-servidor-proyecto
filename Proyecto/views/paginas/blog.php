9d3445563a4111f6535345f4a61f2f5e21a4e88d
unknown <amay90782@ufide.ac.cr>
2024-07-18 19:57:43 +0200

    inicio de las paginas php y scss para blogs

---
+++ Proyecto/views/paginas/blog.php
@@ -0,0 +1,91 @@
+<main class="contenedor seccion contenido-centrado">
+    <h1>Nuestro Blog</h1>
+
+    <article class="entrada-blog">
+        <div class="imagen">
+            <picture>
+                <img src="src/img/blog1.jpg" alt="Imagen Blog" />
+            </picture>
+        </div>
+        <div class="texto-entrada">
+            <a href="/entrada">
+                <h4>Terraza en el techo de tu casa</h4>
+                <p class="informacion-meta">
+                    Escrito el: <span>20/10/2022</span> por: <span>Admin</span>
+                </p>
+
+                <p>
+                    Consejos para construir una terraza en el techo de tu casa con
+                    los mejores materiales ahorrando dinero
+                </p>
+            </a>
+        </div>
+
+    </article>
+
+    <article class="entrada-blog">
+        <div class="imagen">
+            <picture>
+                <img src="src/img/blog2.jpg" alt="Imagen Blog" />
+            </picture>
+        </div>
+        <div class="texto-entrada">
+            <a href="/entrada">
+                <h4>Guía para la decoración de tu hogar</h4>
+                <p class="informacion-meta">
+                    Escrito el: <span>20/10/2022</span> por: <span>Admin</span>
+                </p>
+
+                <p>
+                    Maximiza el espacio en tu hogar con esta guía, aprende a
+                    combinar muebles y colores para darle vida a tu espacio
+                </p>
+            </a>
+        </div>
+
+    </article>
+
+    <article class="entrada-blog">
+        <div class="imagen">
+            <picture>
+                <img src="src/img/blog3.jpg" alt="Imagen Blog" />
+            </picture>
+        </div>
+        <div class="texto-entrada">
+            <a href="/entrada">
+                <h4>Terraza en el techo de tu casa</h4>
+                <p class="informacion-meta">
+                    Escrito el: <span>20/10/2022</span> por: <span>Admin</span>
+                </p>
+
+                <p>
+                    Consejos para construir una terraza en el techo de tu casa con
+                    los mejores materiales ahorrando dinero
+                </p>
+            </a>
+        </div>
+
+    </article>
+
+    <article class="entrada-blog">
+        <div class="imagen">
+            <picture>
+                <img src="src/img/blog4.jpg" alt="Imagen Blog" />
+            </picture>
+        </div>
+        <div class="texto-entrada">
+            <a href="/entrada">
+                <h4>Guía para la decoración de tu hogar</h4>
+                <p class="informacion-meta">
+                    Escrito el: <span>20/10/2022</span> por: <span>Admin</span>
+                </p>
+
+                <p>
+                    Maximiza el espacio en tu hogar con esta guía, aprende a
+                    combinar muebles y colores para darle vida a tu espacio
+                </p>
+            </a>
+        </div>
+
+    </article>
+</main>
